726268e98dbeb7483e8e0a3f08bf37516bdd3c81
Tracey Turner <traceyturner@Traceys-MacBook-Air.local>
2023-04-05 17:58:51 +0200

    Ready to deploy frontend

--- src/test/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { Layout } from "../components/Layout";
-import "@testing-library/jest-dom/extend-expect";
-
-test('should render layout component', () => {
-    render(<Layout/>);
-    const layoutElement = screen.getByTestId('layout');
-    expect(layoutElement).toBeInTheDocument();
-})
